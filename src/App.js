2e3df570ffb04eb5f6b6d0ae72228630c702dcfd
devdave0 <dbchung24@gmail.com>
2020-11-14 17:01:15 -0500

    Navbar links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 import './App.css';
 
 class App extends React.Component {
@@ -30,7 +33,19 @@ class App extends React.Component {
   render(){
     return (
       <Router>
-        <div>Hi</div>
+        <Container className="p-0" fluid={true}>
+          <Navbar className='border-bottom'>
+            <Navbar.Brand>David Chung</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav>
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
       </Router>
     )
   }
