86dafbf9a9ae87ae2d3236d0203384fbadb1645f
devdave0 <dbchung24@gmail.com>
2020-11-14 17:11:58 -0500

    navbar finished

--- src/App.js
+++ src/App.js
@@ -34,11 +34,11 @@ class App extends React.Component {
     return (
       <Router>
         <Container className="p-0" fluid={true}>
-          <Navbar className='border-bottom'>
+          <Navbar className='border-bottom' bg='transparent' expand='lg'>
             <Navbar.Brand>David Chung</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbar-toggle"/>
+            <Navbar.Toggle className='border-0' aria-controls="navbar-toggle"/>
             <Navbar.Collapse id="navbar-toggle">
-              <Nav>
+              <Nav className='ml-auto'>
                 <Link className="nav-link" to="/">Home</Link>
                 <Link className="nav-link" to="/about">About</Link>
                 <Link className="nav-link" to="/contact">Contact</Link>
