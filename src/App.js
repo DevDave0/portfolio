abeccc72990e3292ba24b835a3d932a358c5086b
devdave0 <dbchung24@gmail.com>
2020-11-17 22:41:21 -0500

    Home page done

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
         {title: "Contact", path: '/contact'},
       ],
       home: {
-        title: 'Resilient',
+        title: 'David Chung',
         subTitle: 'Projects that make a difference',
         text: 'Checkout my projects below'
       },
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,8 +4,8 @@ import CardInfo from '../components/CardInfo'
 const Card = (props) => {
 
     return(
-        <div>
-            <img className="" src={props.item.imgSrc} alt={props.item.imgSrc} />
+        <div className="d-inline-block d-card" onClick={(e) => props.click(props.item)}>
+            <img className="d-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
             { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} /> }
         </div>
     )
