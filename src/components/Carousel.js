7263793c1c481041ad621d4d4abe033a08e8bb1f
devdave0 <dbchung24@gmail.com>
2020-11-17 19:12:43 -0500

    handle card click function

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -35,6 +35,24 @@ class Carousel extends React.Component {
         }
     }
 
+    handleCardClick = (id, card) => {
+        let items = [...this.state.items];
+
+        items[id].selected = items[id].selected ? false : true;
+
+        items.forEach(item => {
+            if(item.id !== id){
+                item.selected = false;
+            }
+        })
+
+        this.setState({
+            items
+        })
+    }
+
+    
+
     render() {
         return(
             <p>Carousel works!</p>
