a5087d83a460e858f32d5ad8cdcf607623227b58
devdave0 <dbchung24@gmail.com>
2020-11-18 16:39:58 -0500

    about page done

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Content from '../components/Content';
+import Hero from '../components/Hero';
 
 const AboutPage = (props) => {
     
 
     return(
-        <p>About Page</p>
+        <div>
+            <Hero title={props.title} />
+
+            <Content>
+            <p>Hello, my name is David. I'm a full stack engineer with experience in React, Redux, Vanilla Javascript, Ruby, Ruby on Rails, PostGreSQL, and Node.js.</p>
+
+            <p>My dream is to one day start my own coffee business and become an entrepreneur.</p>
+
+            <p>I'm constantly learning new things. currently those things include gaining more experience with Node.js, React, and Angular.</p>
+
+            <p>My latest project, Obsido, is a budgeting web application. You can check it out <a href="https://github.com/DevDave0/obsido-frontend" target="_blank" rel="noopener noreferrer">here</a>, or on the homepage. It is built with React, Redux, Chart JS, and Ruby on Rails.</p>
+
+            <p>When I'm not learning something new chances are I'm playing basketball or ultimate frisbee.</p>
+            </Content>
+        </div>
     )
 }
 
